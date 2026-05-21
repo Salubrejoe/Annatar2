9145d3ec384cfe23ecb5aa80571de5cf0f7402fa
Licurgen <licurgen@icloud.com>
2026-05-21 09:15:07 +0100

    Initial Commit

---
+++ Annatar2/Annatar2App.swift
@@ -0,0 +1,17 @@
+//
+//  Annatar2App.swift
+//  Annatar2
+//
+//  Created by Licurgen on 21/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct Annatar2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
